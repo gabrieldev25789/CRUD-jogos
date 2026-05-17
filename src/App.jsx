e3662cc1de8ce0cf77b9a0241feaf3869c01c82d
Gabriel <gabrielsb1090@gmail.com>
2026-05-17 01:41:20 -0300

    Criando usuariosCadastrados e passando como prop

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { Routes, Route, Navigate } from 'react-router-dom'
 import CriaConta from './pages/CriaConta'
 import GameVault from './pages/GameVault'
 
+  const usuariosCadastrados = []
+
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<CriaConta />} />
+      <Route path="/" element={<CriaConta usuarios={usuariosCadastrados}/>} />
       <Route path="/app" element={<GameVault />} />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
